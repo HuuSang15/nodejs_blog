8a84472c12bdfed3468868729014d7e2be9f624b
HuuSang15 <phamhuusang24@gmail.com>
2023-04-21 17:30:02 +0700

    Format Code

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
 const newsRouter = require('./news');
 const siteRouter = require('./site');
 
-function route(app) {
+      function route(app) {
     // Action  --> Dispatcher --> Functions Handler
-    app.use('/news', newsRouter);
+                app.use('/news', newsRouter);
 
-    app.use('/', siteRouter);
+             app.use('/', siteRouter);
 }
 
-module.exports = route;
+        module.exports = route;
