8a1436e3ae2689c75bbf2e1ddcbb85e17c0b64cb
HuuSang15 <phamhuusang24@gmail.com>
2023-04-21 15:46:59 +0700

    Router/Controllers

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+const newsRouter = require('./news');
+const siteRouter = require('./site');
+
+
+function route(app) {
+    // Action  --> Dispatcher --> Functions Handler
+    app.use('/news', newsRouter);
+    
+    app.use('/', siteRouter);
+}
+
+module.exports = route;
